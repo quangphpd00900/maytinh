9305daea864c60bb61d1a006fbd8f2328ab0762e
sangtdpd00871 <sangtdpd00872@fpt.edu.vn>
2015-08-11 23:42:41 +0700

    ASSIGMENT

---
+++ admin/views/home.php
@@ -0,0 +1,38 @@
+					<!--[if !IE]>start title wrapper<![endif]-->
+				<div class="section">
+                    <div class="title_wrapper">
+						<span class="title_wrapper_top"></span>
+						<div class="title_wrapper_inner">
+							<span class="title_wrapper_middle"></span>
+							<div class="title_wrapper_content">
+								<h2 class="menu_title">Lời chào</h2>
+							</div>
+						</div>
+						<span class="title_wrapper_bottom"></span>
+					</div>
+					<!--[if !IE]>end title wrapper<![endif]-->
+					<!--[if !IE]>start section content<![endif]-->
+					<div class="section_content">
+						<span class="section_content_top"></span>
+						
+						<div class="section_content_inner">
+							nội dung
+                            <div class="welcome_right">
+                            	<img src="<?php echo base_url; ?>public/admin/images/admin.jpg" width="150" style="padding:2px;border:1px solid #CCC;" />
+                            </div>
+                            <div class="cls" style="padding:5px;border-bottom:1px dashed #CCC;"></div>
+                            <div class="support"> 
+                            	<div class="support_title">
+                                	<h2 class="menu_title">Thông tin hỗ trợ</h2>
+                                </div>
+                                <div class="infomartion">
+                                	<p>Full name 	&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; : Phạm Hồng Quang</p>
+                                    <p>Phone number :096 8289 061</p>
+                                    <p>Email		&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;: quangphpd00900@gmail.com</p>
+                                    
+                                </div>
+                            </div>
+						</div>
+						<span class="section_content_bottom"></span>
+					</div>
+                </div>
